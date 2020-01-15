06d429d08b20df6519d87fae290387c041b5cb75
Rizal Ardhi Rahmadani <reyzeal@users.noreply.github.com>
2020-01-16 00:06:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import multiprocessing
 import os
 import re
 import time
+from urllib import parse
+
 from combine import combine
 import requests
 import xlsxwriter
@@ -48,12 +50,7 @@ if THREAD <= 0:
 if THREAD < CPU:
     CPU = THREAD
 
-req = requests.session()
-ops = Options()
-ops.add_argument('--headless')
-ops.add_argument('--ignore-certificate-errors')
-ops.add_argument('--ignore-ssl-errors')
-ops.add_argument("--log-level=3")
+
 
 
 def scrap(url):
@@ -122,15 +119,27 @@ def overview(url):
     return product
 
 
-def url_filter(url):
-    f = 'http://www.alibaba.com/product-detail/'
-    o = f'http:{url}'
+def url_filter(turl):
+    global url
+    if CRAWLER_TYPE == 'general':
+        f = 'http://www.alibaba.com/product-detail/'
+    else:
+        ii = re.findall(r'productlist-\d+.html\?.*',url)[0]
+        f = re.findall(r'.*/productlist',url)[0].replace('/productlist','')
+        return f+turl
+    o = f'http:{turl}'
     if f in o:
         return o
     return None
 
 
 target = '.organic-gallery-offer-outter.J-offer-wrapper a'
+req = requests.session()
+ops = Options()
+ops.add_argument('--headless')
+ops.add_argument('--ignore-certificate-errors')
+ops.add_argument('--ignore-ssl-errors')
+ops.add_argument("--log-level=3")
 driver = webdriver.Chrome(options=ops, service_log_path='NUL')
 
 
@@ -154,8 +163,10 @@ def worker(val, worksheet, row, page):
         print(f'Page {page}-Worker {x + 1}', f'remaining jobs:{len(lists)}', val[0])
         result = overview(val[0])
         result.update({'Minimum MOQ': str(val[4]).replace('(Min Order)', '')})
-        result.update({'ID': re.findall(r'(\d+)\.html', result.get('Productlink'))[0].replace('.html', '')})
-
+        if CRAWLER_TYPE == 'general':
+            result.update({'ID': re.findall(r'(\d+)\.html', result.get('Productlink'))[0].replace('.html', '')})
+        else:
+            result.update({'ID': re.findall(r'/product/(\d+)', result.get('Productlink'))[0].replace('/product/', '')})
         for j, val in enumerate(head_data):
             worksheet.write(row, j, result.get(val, ''))
     x = worker_pool.get(page, 0)
@@ -185,14 +196,31 @@ def periodic(lists, page=1):
 
 page = PAGE_START
 status = True
+CRAWLER_TYPE = 'shop'
+def url_detection(url):
+    if re.match(r'(www\.|.*)alibaba\.com/products/', url):
+        return 'general'
+    return 'shop'
 while status or _thread._count() > 0:
     if PAGES is None:
         pass
     elif page > PAGES:
         status = False
         continue
-    print('crawling page', page, ' of ' + url + f'&page={page}')
-    driver.get(url + f'&page={page}')
+    url_parsed = parse.urlparse(url)
+    params = parse.parse_qs(url_parsed.query)
+    if url_detection(url) == 'general':
+        params = [f'{i}={(params.get(i)[0])}' for i in params.keys()]
+        params = "&".join(params)
+        _target = f'{url_parsed.scheme}://{url_parsed.netloc}{url_parsed.path}?{params}'
+    else:
+        params = [f'{i}={(params.get(i)[0])}' for i in params.keys()]
+        params = "&".join(params)
+        _page = f'/productlist-{page}.html'
+        _target = f'{url_parsed.scheme}://{url_parsed.netloc}{_page}?{params}'
+
+    print('crawling page', page, ' of ' + _target)
+    driver.get(_target)
     elements = driver.find_element_by_css_selector('body')
     elements.send_keys(Keys.CONTROL, Keys.END)
     elements.send_keys(Keys.CONTROL + Keys.END)
@@ -203,40 +231,69 @@ while status or _thread._count() > 0:
     elements.send_keys(Keys.CONTROL, Keys.END)
     elements.send_keys(Keys.CONTROL + Keys.END)
     time.sleep(2)
-    execute_script(
-        'x = document.querySelectorAll(".organic-gallery-offer-outter.J-offer-wrapper");window._y = []; for(i=0;i<x.length;i++){window._y.push(x[i])};')
-    execute_script('for(i=0;i<window._y.length;i++){window._y[i].style="position:fixed;top:0;left:0"}')
-    links = execute_script(
-        'window._items = [];for(i=0;i<window._y.length;i++){window._items.push(window._y[i].querySelector("a").getAttribute("href"))}',
-        True)
-    imgs = execute_script(
-        'window._items = [];for(i=0;i<window._y.length;i++){window._items.push(window._y[i].querySelector("img").getAttribute("src"))}',
-        True)
-    titles = execute_script(
-        'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".organic-gallery-title__content"); if(temp) window._items.push(temp.innerText); else window._items.push("")}',
-        True)
-    offers = execute_script(
-        'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".gallery-offer-price"); if(temp) window._items.push(temp.innerText); else window._items.push("")}',
-        True)
-    orders = execute_script(
-        'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".gallery-offer-minorder"); if(temp) window._items.push(temp.innerText); else window._items.push("")}',
-        True)
-    companies = execute_script(
-        'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".organic-gallery-offer__seller-company"); if(temp) window._items.push(temp.innerText); else window._items.push("")}',
-        True)
-    countries = execute_script(
-        'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".seller-tag__country"); if(temp) window._items.push(temp.getAttribute("title")); else window._items.push("")}',
-        True)
-    years = execute_script(
-        'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".seller-tag__year"); if(temp) window._items.push(temp.innerText); else window._items.push("")}',
-        True)
-    ratings = execute_script(
-        'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".seb-supplier-review__score"); if(temp) window._items.push(temp.innerText); else window._items.push("")}',
-        True)
+    if url_detection(url) == 'general':
+        CRAWLER_TYPE = 'general'
+        execute_script(
+            'x = document.querySelectorAll(".organic-gallery-offer-outter.J-offer-wrapper");window._y = []; for(i=0;i<x.length;i++){window._y.push(x[i])};')
+        execute_script('for(i=0;i<window._y.length;i++){window._y[i].style="position:fixed;top:0;left:0"}')
+        links = execute_script(
+            'window._items = [];for(i=0;i<window._y.length;i++){window._items.push(window._y[i].querySelector("a").getAttribute("href"))}',
+            True)
+        imgs = execute_script(
+            'window._items = [];for(i=0;i<window._y.length;i++){window._items.push(window._y[i].querySelector("img").getAttribute("src"))}',
+            True)
+        titles = execute_script(
+            'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".organic-gallery-title__content"); if(temp) window._items.push(temp.innerText); else window._items.push("")}',
+            True)
+        offers = execute_script(
+            'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".gallery-offer-price"); if(temp) window._items.push(temp.innerText); else window._items.push("")}',
+            True)
+        orders = execute_script(
+            'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".gallery-offer-minorder"); if(temp) window._items.push(temp.innerText); else window._items.push("")}',
+            True)
+        companies = execute_script(
+            'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".organic-gallery-offer__seller-company"); if(temp) window._items.push(temp.innerText); else window._items.push("")}',
+            True)
+        countries = execute_script(
+            'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".seller-tag__country"); if(temp) window._items.push(temp.getAttribute("title")); else window._items.push("")}',
+            True)
+        years = execute_script(
+            'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".seller-tag__year"); if(temp) window._items.push(temp.innerText); else window._items.push("")}',
+            True)
+        ratings = execute_script(
+            'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".seb-supplier-review__score"); if(temp) window._items.push(temp.innerText); else window._items.push("")}',
+            True)
+    else:
+        print('SHOP')
+        execute_script(
+            'x = document.querySelectorAll(".icbu-product-card.vertical.large.product-item");window._y = []; for(i=0;i<x.length;i++){window._y.push(x[i])};')
+        execute_script('for(i=0;i<window._y.length;i++){window._y[i].style="position:fixed;top:0;left:0"}')
+        links = execute_script(
+            'window._items = [];for(i=0;i<window._y.length;i++){window._items.push(window._y[i].querySelector("a").getAttribute("href"))}',
+            True)
+        imgs = execute_script(
+            'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector("img");if(temp) window._items.push(window._y[i].querySelector("img").getAttribute("src")); else window._items.push("");}',
+            True)
+        titles = execute_script(
+            'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".title-con"); if(temp) window._items.push(temp.innerText); else window._items.push("")}',
+            True)
+        offers = execute_script(
+            'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".price"); if(temp) window._items.push(temp.getAttribute("title")); else window._items.push("")}',
+            True)
+        orders = execute_script(
+            'window._items = [];for(i=0;i<window._y.length;i++){temp = window._y[i].querySelector(".moq"); if(temp) window._items.push(temp.getAttribute("title")); else window._items.push("")}',
+            True)
+        companies = ['' for i in orders]
+        countries = ['' for i in orders]
+        years = ['' for i in orders]
+        ratings = ['' for i in orders]
+        print(offers)
+
     while _thread._count() != 0:
         time.sleep(5)
     lists = list(zip(links, imgs, titles, offers, orders, companies, countries, years, ratings))
     lists = [[url_filter(i[0])] + list(i)[1:] for i in lists if url_filter(i[0]) is not None]
+
     with open(f'{DIRNAME}/temp/{FILENAME}{page}.json', 'w') as f:
         json.dump(lists, f)
     x = driver.execute_script('return document.querySelector(".pages-next.disabled")?1:0')
